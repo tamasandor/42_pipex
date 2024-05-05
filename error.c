207287895dde59da90a31de5e35a3de5b9d692c7
tamasandor <tamas.andor@yahoo.com>
2024-05-06 01:51:47 +0200

    extract path

--- error.c
+++ error.c
@@ -5,31 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/02 00:30:20 by atamas            #+#    #+#             */
-/*   Updated: 2024/05/02 19:14:02 by atamas           ###   ########.fr       */
+/*   Created: 2024/05/05 22:42:20 by atamas            #+#    #+#             */
+/*   Updated: 2024/05/06 01:50:25 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include "pipex.h"
 
-int	main(int argc, char *argv[])
+// test
+#include <stdio.h>
+
+char	*extract_path(char *envp[])
 {
-	int	firstfile;
-	int	secondfile;
+	char	*path;
+	int		i;
 
-	if (argc < 5)
+	i = 0;
+	while (envp[i])
 	{
-		write(2, "Not enough arguments\n", 22);
-		exit(1);
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			path = ft_strchr(envp[i], '/');
+			return (path);
+		}
+		i++;
 	}
-	firstfile = open(argv[1], O_RDONLY);
-	secondfile = open(argv[4], O_WRONLY | O_CREAT, 0666);
-	if (firstfile == -1 || secondfile == -1)
+	return (NULL);
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	int			first;
+	const char	*path = extract_path(envp);
+	/* first = open(argv[1], O_RDONLY);
+	if (first == -1)
 	{
 		write(2, "Error opening the file\n", 24);
 		exit(1);
-	}
+	} */
 }
